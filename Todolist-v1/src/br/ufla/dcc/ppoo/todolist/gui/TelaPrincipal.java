5411946f96d892de10949cd4c16c13c53658f25b
Paulo <Paulo@177.105.60.111>
2018-05-08 22:38:46 -0300

    update todolis

--- Todolist-v1/src/br/ufla/dcc/ppoo/todolist/gui/TelaPrincipal.java
+++ Todolist-v1/src/br/ufla/dcc/ppoo/todolist/gui/TelaPrincipal.java
@@ -3,6 +3,7 @@ package br.ufla.dcc.ppoo.todolist.gui;
 import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Insets;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -42,16 +43,16 @@ public class TelaPrincipal extends JFrame {
     public TelaPrincipal() {
         // Define o título da tela
         super("Lista de Tarefas");
-        
+
         // Define que fechar a janela, a execução aplicação será encerrada
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         // Evita que a tela possa ser redimensionada pelo usuário
         setResizable(false);
-        
+
         // Invoca o método que efetivamente constrói a tela
         construirTela();
-        
+
         // Redimensiona automaticamente a tela, com base nos componentes existentes na mesma
         pack();
     }
@@ -60,54 +61,61 @@ public class TelaPrincipal extends JFrame {
         // Instancia os objetos de layout da tela
         gbc = new GridBagConstraints();
         gbl = new GridBagLayout();
-        
+
         // Configura o layout da tela
         setLayout(gbl);
 
         // Instancia os objetos referentes aos componentes da tela
         lbDescricao = new JLabel("Tarefa");
         lbDeadline = new JLabel("Deadline");
-        
+
         tfDescricao = new JTextField(29); // 15 refere-se ao tamanho da caixa de texto 
         tfDeadline = new JTextField(7); // 15 refere-se ao tamanho da caixa de texto
-        
+
         btCriarNovo = new JButton("Nova tarefa");
         btCriarNovo.setEnabled(false); // define que o botão não está habilitado (não pode ser clicado)
-        btSalvar = new JButton("Salvar");        
+        btSalvar = new JButton("Salvar");
         btRemover = new JButton("Remover");
         btRemover.setEnabled(false); // define que o botão não está habilitado (não pode ser clicado)
-        
+
         // Instancia o painel (container) de botões e adiciona os botões a ele
-        painelBotoes = new JPanel();
+        painelBotoes = new JPanel(new GridLayout(1, 3, 5, 5));
         painelBotoes.add(btSalvar);
         painelBotoes.add(btCriarNovo);
         painelBotoes.add(btRemover);
 
-        // Constrói a tabela de dados 
+        // Constrói o modelo de dados 
         mdDados = new DefaultTableModel(); // Toda tabela possui um modelo de dados, que é onde ficam as informações exibidas pela tabela
-        tbTarefas = new JTable(mdDados);
 
         // Adicionando colunas ao modelo de dados. 
         mdDados.addColumn("Tarefa");
         mdDados.addColumn("Deadline");
-        
-        // Adiciona uma linha de dados "fake" ao modelo de dados
-        mdDados.addRow(new Object[]{"Pagar aluguel", "05/05/2018"});
-        
+
+        // Adiciona linhas de dados "fake" ao modelo de dados
+        for (int i = 0; i < 70; i++) {
+            String[] dadosFake = new String[2];
+            dadosFake[0] = "Tarefa " + (i + 1);
+            dadosFake[1] = "09/05/2018";
+            mdDados.addRow(dadosFake);
+        }
+
+        // Constrói a tabela, com base no modelo de dados
+        tbTarefas = new JTable(mdDados);
+
         // Configura o tamanho das colunas da tabela
         tbTarefas.getColumnModel().getColumn(0).setMaxWidth(400);
         tbTarefas.getColumnModel().getColumn(1).setMaxWidth(100);
-        
+
         // Uma tabela precisam estar inserida em um componente JScrollPane, para que barras de rolagem sejam adicionadas a ela
         painelTarefas = new JScrollPane(tbTarefas);
 
         // Adicionando os componentes recém-criados à tela
-        adicionarComponente(lbDescricao, GridBagConstraints.EAST, GridBagConstraints.BOTH, 0, 0, 1, 1);
-        adicionarComponente(lbDeadline, GridBagConstraints.EAST, GridBagConstraints.BOTH, 0, 1, 1, 1);
+        adicionarComponente(lbDescricao, GridBagConstraints.CENTER, GridBagConstraints.NONE, 0, 0, 1, 1);
+        adicionarComponente(lbDeadline, GridBagConstraints.CENTER, GridBagConstraints.NONE, 0, 1, 1, 1);
         adicionarComponente(tfDescricao, GridBagConstraints.EAST, GridBagConstraints.BOTH, 1, 0, 1, 1);
         adicionarComponente(tfDeadline, GridBagConstraints.EAST, GridBagConstraints.BOTH, 1, 1, 1, 1);
-        adicionarComponente(painelTarefas, GridBagConstraints.CENTER, GridBagConstraints.BOTH, 2, 0, 2, 1);
-        adicionarComponente(painelBotoes, GridBagConstraints.CENTER, GridBagConstraints.BOTH, 3, 0, 2, 1);
+        adicionarComponente(painelBotoes, GridBagConstraints.CENTER, GridBagConstraints.BOTH, 2, 0, 2, 1);
+        adicionarComponente(painelTarefas, GridBagConstraints.CENTER, GridBagConstraints.BOTH, 3, 0, 2, 1);
 
     }
 
